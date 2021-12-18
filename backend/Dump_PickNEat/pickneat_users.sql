0ed8edd1b66fe188bda5f19a50f2afa3ec4c76ad
David <david-bourrel@hotmail.fr>
2021-12-18 18:46:28 +0100

    DB updated

--- backend/Dump_PickNEat/pickneat_users.sql
+++ backend/Dump_PickNEat/pickneat_users.sql
@@ -29,7 +29,6 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `age` int NOT NULL,
   `hashedPassword` varchar(255) NOT NULL,
-  `refreshToken` varchar(255) DEFAULT NULL,
   `roles_id` varchar(30) NOT NULL,
   PRIMARY KEY (`roles_id`,`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
@@ -42,7 +41,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('2d522918353f3946e02a0866719a50','Admin','Admin','admin@pickandeat.com',100,'$argon2id$v=19$m=65536,t=5,p=1$Oh23AijOZdczNPjRAtblzg$9YBhUU2jlCRqtjcXwgIqQg7B9qe5l0P8of1e//KXCYo','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjJkNTIyOTE4MzUzZjM5NDZlMDJhMDg2NjcxOWE1MCIsImVtYWlsIjoiYWRtaW5AcGlja2FuZGVhdC5jb20iLCJpYXQiOjE2MzkzMDQ2NDYsImV4cCI6MTYzOTM5MTA0Nn0.YTe82MRf-yatAqY8ri8T2QXy52aYbT77oPsZ33q4bkc','1'),('1d793aa3481e1966f5770b021ddcdf','Julie','Garcia','julie@hotmail.fr',28,'$argon2id$v=19$m=65536,t=5,p=1$58LQtLxB5q+Hp0g/d3YdHA$Ywco0Ay/Hi/ORabvtkq16Qg6HWYlSxdoGwe0X5IaYt4',NULL,'2'),('aa5a0575cf277f9e4125df51289e38','Thomas','Garcia','thomas@hotmail.fr',26,'$argon2id$v=19$m=65536,t=5,p=1$pmA6vHMf/+I5cNbQZehHnw$rjqPxKZE30VwcuPxyREwNpbpzSORGgtUeR99FenBbhw',NULL,'2'),('e70c1e9fdefc4e34099baf19e8e4ae','Manon','Garcia','manon@hotmail.fr',52,'$argon2id$v=19$m=65536,t=5,p=1$tmp/7IArKNxqXN0Z/ocxQA$7E7AUjIKqB0lkZMVr/oE3+akoEQljJZfOPtvwkKojck',NULL,'2');
+INSERT INTO `users` VALUES ('2d522918353f3946e02a0866719a50','Admin','Admin','admin@pickandeat.com',100,'$argon2id$v=19$m=65536,t=5,p=1$Oh23AijOZdczNPjRAtblzg$9YBhUU2jlCRqtjcXwgIqQg7B9qe5l0P8of1e//KXCYo','1'),('1d793aa3481e1966f5770b021ddcdf','Julie','Garcia','julie@hotmail.fr',28,'$argon2id$v=19$m=65536,t=5,p=1$58LQtLxB5q+Hp0g/d3YdHA$Ywco0Ay/Hi/ORabvtkq16Qg6HWYlSxdoGwe0X5IaYt4','2'),('aa5a0575cf277f9e4125df51289e38','Thomas','Garcia','thomas@hotmail.fr',26,'$argon2id$v=19$m=65536,t=5,p=1$pmA6vHMf/+I5cNbQZehHnw$rjqPxKZE30VwcuPxyREwNpbpzSORGgtUeR99FenBbhw','2'),('e70c1e9fdefc4e34099baf19e8e4ae','Manon','Garcia','manon@hotmail.fr',52,'$argon2id$v=19$m=65536,t=5,p=1$tmp/7IArKNxqXN0Z/ocxQA$7E7AUjIKqB0lkZMVr/oE3+akoEQljJZfOPtvwkKojck','2');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-12 12:09:53
+-- Dump completed on 2021-12-18 18:42:41
