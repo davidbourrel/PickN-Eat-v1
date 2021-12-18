f5c02609fa2ca2d649049dc89be782ea156a6904
David <david-bourrel@hotmail.fr>
2021-12-18 19:48:49 +0100

    verifyJWT changed

--- backend/middlewares/verifyJWT.js
+++ backend/middlewares/verifyJWT.js
@@ -3,15 +3,36 @@ require('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
   // Bearer token
-  const authHeader = req.headers['authorization'];
-  if (!authHeader)
+
+  const { headers } = req;
+  if (!headers.authorization)
     return res.status(401).json({
-      message: 'Invalid token',
+      message: 'Missing Authorization header',
     });
 
-  const token = authHeader.split(' ')[1];
+  const parts = headers.authorization.split(' ');
+
+  if (parts.length !== 2) {
+    return res.status(403).json({
+      message: `Header format is Authorization: ${process.env.ACCESS_TOKEN_TYPE} token`,
+    });
+  }
+
+  const scheme = parts[0];
+  const token = parts[1];
+
+  if (
+    process.env.ACCESS_TOKEN_TYPE.toLowerCase() !== scheme.toLowerCase() ||
+    !token
+  ) {
+    return res.status(403).json({
+      message: `Header format is Authorization: ${process.env.ACCESS_TOKEN_TYPE} token`,
+    });
+  }
+
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403); //invalid token
+    if (err)
+      return res.status(403).json({ messages: `invalid token : ${err}` });
     next();
   });
 };
