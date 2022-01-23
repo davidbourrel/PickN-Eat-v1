d48d2d28384367a01aa9389a18f4095f88682ef2
David <david-bourrel@hotmail.fr>
2022-01-23 12:45:39 +0100

    renaming signUp

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const registerController = require('../controllers/register');
 
-router.post('/', registerController.handleRegister);
+router.post('/', registerController.signUp);
 
 module.exports = router;
