fb91093d6f2c064287adfb19307210055bcd2c57
David <david-bourrel@hotmail.fr>
2022-03-06 10:17:23 +0100

    try fix register bug

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -2,17 +2,18 @@ const User = require('../models/users');
 
 const signUp = async (req, res) => {
   const { email, password } = req.body;
+
   if (!email || !password)
     return res.status(400).json({ message: 'Email or password is required.' });
 
-  // check for duplicate user in the database
-  const [retrieveUsers] = await User.getAll();
-  const duplicate = retrieveUsers.find((person) => person.email === email);
-
-  if (duplicate)
-    return res.status(409).json({ message: 'This email is already used.' });
-
   try {
+    // check for duplicate user in the database
+    const [retrieveUsers] = await User.getAll();
+    const duplicate = retrieveUsers.find((person) => person.email === email);
+
+    if (duplicate)
+      return res.status(409).json({ message: 'This email is already used.' });
+
     await User.createOne(req.body);
     res.status(201).json({ success: 'New user created!' });
   } catch (err) {
